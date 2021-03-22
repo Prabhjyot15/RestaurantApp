9f3e9ae5bdc6d9d541820ea4e04df7a49da770e9
Prabhjyot15 <bamrahprabhjyot@gmail.com>
2021-03-22 20:01:14 +0530

    Updating cart Functionality

--- src/app/cart/cart-list/cart-list.component.ts
+++ src/app/cart/cart-list/cart-list.component.ts
@@ -17,5 +17,35 @@ cartList : CartItem[] = []
   console.log( this.cartList )
 });
   }
+  reduceQuantity(cartItem){
+    this.cartList.forEach(item=>{
+      if(item.itemName == cartItem.itemName){
+        if(item.itemQuantity == 1){
+          let index = this.cartList.findIndex(obj => obj.itemName== cartItem.itemName);
+          this.cartList.splice(index,1);
+        }else{
+          item.itemQuantity = item.itemQuantity-1;
+        }        
+      }
+    });
 
+    this.dataStateService.cartItems.next(this.cartList);
+  }
+
+  addQuantity(cartItem){
+    this.cartList.forEach(item=>{
+      if(item.itemName == cartItem.itemName){
+        item.itemQuantity = item.itemQuantity+1;
+      }
+    });
+
+    this.dataStateService.cartItems.next(this.cartList);
+  }
+
+  deleteItem(item){
+    console.log(item)
+   let index = this.cartList.findIndex(obj => obj.itemName== item.itemName);
+   this.cartList.splice(index,1);
+   this.dataStateService.cartItems.next(this.cartList);
+  }
 }
